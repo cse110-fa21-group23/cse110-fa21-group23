e34a11f6d73c1bcceaf9a56d35bf6e28200ef6ed
nhilikeknee <56176648+nhilikeknee@users.noreply.github.com>
2021-11-04 17:23:24 -0700

    added menu and basic settings page

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -8,8 +8,57 @@ document.addEventListener('keydown', function (event) {
 
 function search() {
     //TODO: remove this alert, implement real search
-    let searchQuery = document.getElementById('searchQuery').value;
+    let searchQuery = document.getElementById('search-query').value;
     alert(searchQuery);
 
 }
 
+function init() {
+    showHome();
+}
+
+
+var $SOMenuVisibility = "hidden";
+function toggleMenu() {
+    var menuIcon = document.getElementById("menu-icon");
+    menuIcon.classList.toggle("change");
+
+    var slideOverMenu = document.getElementById("slide-over-menu");
+
+    if ($SOMenuVisibility == "hidden") {
+        slideOverMenu.style.transform = "translate(100%)";
+        $SOMenuVisibility = "visible";
+    }
+    else {
+        slideOverMenu.style.transform = "translate(-100%)";
+        $SOMenuVisibility = "hidden";
+    }
+}
+
+function showSettings() {
+    hideHome();
+    var settings = document.getElementById("settings-container");
+    settings.style.transform = "translate(100%)";
+}
+
+function hideSettings() {
+    var settings = document.getElementById("settings-container");
+    settings.style.transform = "translate(-100%)";
+}
+
+function showHome() {
+    hideSettings();
+    var search = document.getElementById("search");
+    search.style.visibility = "visible";
+}
+
+function hideHome() {
+    var search = document.getElementById("search");
+    search.style.visibility = "hidden";
+}
+
+
+function updateSettings() {
+    // TODO: Implement update settings for dietary preferences
+    console.log("update button was pressed");
+}
