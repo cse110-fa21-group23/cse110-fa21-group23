53baf36891e8931f67b8df27495c140e0ebbf47b
David Suzuki <David@suzuki-aloha.com>
2021-11-05 12:32:07 -0700

    card template set, started basics of recipe.js

--- source/scripts/RecipeCard.js
+++ source/scripts/RecipeCard.js
@@ -3,4 +3,66 @@ class RecipeCard extends HTMLElement {
         super();
         this.attachShadow({ mode: 'open' });
     }
+    set data(data) {
+        const styleElement = document.createElement("style");
+        const styles = `{
+            * {
+                font-family: 'Poppins', sans-serif;
+                color: var(--primary);
+                margin: 0;
+                padding: 0;
+            }
+
+            article {
+                align-items: center;
+                border: 1px;
+                border-radius: 8px;
+                display: grid;
+                row-gap: 5px;
+                height: auto;
+                width: 178px;
+                padding: 16px 16px 16px 16px;
+            }
+
+            article > img {
+                border-top-left-radius: 8px;
+                border-top-right-radius: 8px;
+                height: 118px;
+                object-fit: cover;
+                width: calc(100% + 16px);
+            }
+            p.title {
+                font-size: 16px;
+                height: 36px;
+                line-height: 18px;
+                overflow: hidden;
+            }
+
+            div.tags {
+                height: 32px;
+                line-height: 16px;
+                padding-top: 4px;
+            }
+
+        }`; 
+        styleElement.innerHTML = styles;
+        const card = document.createElement('article');
+        
+        this.shadowRoot.appendChild(card);
+        this.shadowRoot.appendChild(styleElem);
+
+        let recipeImage = document.createElement("img");
+        //need to use the api to get the recipe's image src
+        this.card.appendChild(recipeImage);
+
+        let titleElement = document.createElement("p");
+        //need to use the api to get the actual title for the recipe
+        let titleText = "testTitle";
+        titleElement.innerHTML = titleText;
+        this.card.appendChild(titleElement);
+
+        let tagsDiv = document.createElement("div");
+        //need to use the api to get a list of the tags of the dish
+        
+    }
 }
--- source/scripts/script.js
+++ source/scripts/script.js
@@ -13,3 +13,43 @@ function search() {
 
 }
 
+window.addEventListener('DOMContentLoaded', init);
+const recipes = [];
+const recipeData = {};
+
+async function init() {
+    // fetch the recipes and wait for them to load
+    let fetchSuccessful = await fetchRecipes();
+    // console.log(fetchSuccessful)
+    // if they didn't successfully load, quit the function
+    if (!fetchSuccessful) {
+      console.log('Recipe fetch unsuccessful');
+      return;
+    };
+    
+    // Add the first three recipe cards to the page
+    createRecipeCards();
+    // // Make the "Show more" button functional
+    // bindShowMore();
+}
+async function fetchRecipes() {
+    return new Promise((resolve, reject) => {
+
+      
+    //   for (let i = 0; i < recipes.length; i++) {
+        
+    //     fetch(recipes[i])
+    //     .then(response => response.json())
+    //     .then(data => { recipeData["recipe" + i] = data; if (Object.keys(recipeData).length == recipes.length) {
+    //       resolve(true); }  })
+    //     .catch( error => reject(false));
+    //   }
+            
+    });
+}
+function createRecipeCards() {
+    let recipeCard1 = document.createElement("recipe-card");
+    recipeCard1.data = recipeData["recipe"+1];
+    
+    document.getElementById("recipe-cards").appendChild(recipeCard1);
+} 
