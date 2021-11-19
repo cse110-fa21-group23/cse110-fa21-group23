3c638a4ca88adf1fc16d23015f69821c2533a273
karinasanchez04 <46555013+karinasanchez04@users.noreply.github.com>
2021-11-18 19:48:33 -0800

    Update source/scripts/script.js
    Co-authored-by: kjl001 <47440034+kjl001@users.noreply.github.com>

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -137,7 +137,7 @@ function createRecipeCards() {
 
 function bindRecipeCard(recipeCard, pageName) {
     recipeCard.addEventListener('click', e => {
-      if (e.path[0].nodeName == 'A') return;
+        if (e.path[0].nodeName == "A") return;
       router.navigate(pageName, false);
     });
 }
