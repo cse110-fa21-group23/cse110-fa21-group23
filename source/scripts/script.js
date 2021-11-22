fefdf5809b73d6dfa0e2533359ae8a1e002ddbbd
Them Dang <dnthem@gmail.com>
2021-11-22 13:47:18 -0800

    fixed e.path to e.composePath()

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -150,7 +150,7 @@ function createRecipeCards() {
 
 function bindRecipeCard(recipeCard, pageName) {
     recipeCard.addEventListener('click', e => {
-        if (e.path[0].nodeName == "A") return;
+        if (e.composePath()[0].nodeName == "A") return;
         router.navigate(pageName, false);
     });
 }
