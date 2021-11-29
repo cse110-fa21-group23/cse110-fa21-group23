0662989a1b8f78523e1e27c15e1f3cac4f85458a
amiteshksharma <amitesh.sharma.2001@gmail.com>
2021-11-28 19:14:37 -0800

    fixing kevin's bug finds

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -3,6 +3,7 @@ import { fetchRecipes } from "./api_script.js";
 import { Router } from "./Router.js";
 
 let recipeData = {};
+let prevSearch = '';
 
 const router = new Router(function () {
     showHome();
@@ -84,14 +85,21 @@ async function init() {
 function search() {
     // get the search query
     const searchQuery = document.getElementById("search-query").value;
-    // let searchQuery = document.getElementById('search-query').value;
-    // console.log(searchQuery);
-    // console.log(localStorage.getItem("dietaryRestrictions"));
-
     const recipeCardContainer = document.getElementById('recipe-card-container');
 
+    // If it is empty, alert the user it is empty
+    if (!searchQuery) {
+        alert("Please input a search or click a filter below");
+        return false;
+    }
+    
+    // If the prev search hasn't changed, simply keep the results
+    if(prevSearch === searchQuery) return false;
+
+    prevSearch = searchQuery;
     const page = searchQuery;
     router.addPage(page, function () {
+        hideRecipePage()
         hideCategoryCards();
         showRecipeCards();
         showSearchBar();
@@ -117,12 +125,6 @@ function search() {
     let queryStrIntolerances = "";
     if (getIntolerancesRestrictions && getIntolerancesRestrictions.length !== 0) {
         queryStrIntolerances = `&intolerances=${getIntolerancesRestrictions}`
-    }
-
-    // If it is empty, alert the user it is empty
-    if (!searchQuery) {
-        alert("Please input a search or click a filter below");
-        return;
     }
 
     // Fetch the Recipes with the specified queries
