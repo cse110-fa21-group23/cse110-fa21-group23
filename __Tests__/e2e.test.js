c7483bc43663c21990813f5bd76ccd5b41c37ec8
amiteshksharma <amitesh.sharma.2001@gmail.com>
2021-12-01 12:25:31 -0800

    small change

--- __Tests__/e2e.test.js
+++ __Tests__/e2e.test.js
@@ -115,6 +115,7 @@ describe("basic user flow -- searching with a query and clicking a search result
 
       // click the first category card
       await page.$eval("#search-query", el => el.value = "pasta");
+      
       const searchButton = await page.$("#search-button");
       await searchButton.click();
 
