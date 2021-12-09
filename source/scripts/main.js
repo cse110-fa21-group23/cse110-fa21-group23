405ce23533355fa54cf59b4d5efc08f8bfd0c0bd
Christopher Kroon <ckroon@ucsd.edu>
2021-12-09 00:52:46 -0800

    checkNames

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -492,15 +492,50 @@ function appendNewCookBook(newCookBook) {
  * store it to local storage and call appendNewCookBook()
  * 
  */
-function addNewCookBook() {
+ function addNewCookBook() {
     let newCookBook = prompt("Enter new cookbook:");
-    if (newCookBook == "" || newCookBook == null) { return; }
+    if (newCookBook == null) { return; }
+    cookbookName = cookbookName.replace(/\s+/g, ' ').trim();
+    while (processTextSubmitCookbook(cookbookName) === false) {
+        if (cookbookName == "") {
+            cookbookName = prompt("Error: No input detected. Please choose a valid name.");
+        } else {
+            cookbookName = prompt("Error: Another cookbook already has that name. Please choose another.");
+        }
+        if (cookbookName != null) {
+            cookbookName = cookbookName.replace(/\s+/g, ' ').trim();
+        } else {
+            return;
+        }
+    }
+
     appendNewCookBook(newCookBook);
     // update local storage
     let cookBooks = JSON.parse(localStorage.getItem(COOK_BOOKS));
     cookBooks.push(newCookBook);
     localStorage.setItem(COOK_BOOKS, JSON.stringify(cookBooks));
 }
+
+/**
+ * Checks to see if the cookbook name is valid
+ *
+ * @param {string} userInput the cookbook name to check
+ * @return {boolean} true if the name isn't null, empty, or already in use. Otherwise it returns false
+ */
+ function processTextSubmitCookbook(userInput) {
+    // Checks if the input is empty, if so it changes the request text and exits the function
+    if (userInput == "") {
+        return false;
+    }
+    // Checks if the input is the same as another cookbook, if so it changes the request text and exits the function
+    let cookbooks = JSON.parse(localStorage.getItem(COOK_BOOKS));
+    for (let i= 0; i < cookbooks.length; i++) {
+        if (cookbooks[i] == userInput) {
+            return false;
+        }
+    }
+    return true;
+ }
 
 /* end of save new cookbook ====================================================*/
 
